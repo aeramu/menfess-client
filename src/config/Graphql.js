517806b1fc4916db6978da003ef9ef0eda9450de
Sulthan Alam <sulam3010@gmail.com>
2020-09-16 17:37:36 +0700

    [alam] migrating server to heroku

--- src/config/Graphql.js
+++ src/config/Graphql.js
@@ -2,9 +2,10 @@ import ApolloClient from 'apollo-boost'
 import {AsyncStorage} from 'react-native'
 
 export const client = new ApolloClient({
-    uri: 'https://owcc3bfav4.execute-api.ap-southeast-1.amazonaws.com/graphql',
-    //uri: 'https://11kqrmkw35.execute-api.ap-southeast-1.amazonaws.com/graphql',
-    // uri: 'http://192.168.43.82:8000',
+    uri:'http://menfess-server.herokuapp.com', //from heroku
+    //uri: 'https://owcc3bfav4.execute-api.ap-southeast-1.amazonaws.com/graphql', //from aws qiupdeveloper
+    //uri: 'https://11kqrmkw35.execute-api.ap-southeast-1.amazonaws.com/graphql', //from aws nakonigtur
+    // uri: 'http://192.168.43.82:8000', //from local
     request: async (operation) => {
         const id = await AsyncStorage.getItem('id')
         operation.setContext({
